f8d79cfbfef6397ede95b1c8dbb36d675495c80b
RAKOTONDRASOA Nirilala <nirilala@makeitpulse.in.axian-group.com>
2022-07-12 16:39:42 +0300

    Create webservice on unity

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,7 @@
-async function main(unityInstance){
-    try{
-        const res = await axios.get('/token')
-        const data = res.data
-        const user = await axios.get('https://back-immochv2.my-preprod.space/api/immo/v2/public/user_info', {headers: {'Authorization': 'Bearer ' + data.token}})
-        console.log(user.data.data)
-        unityInstance.SendMessage('Main Camera', 'SetUser', JSON.stringify(user.data.data))
-    }catch(e){ alert('Authentication Error!') }
+async function main(instance){
+    const search = new URLSearchParams(location.search)
+    const token = search.get('token')    
+    const user = await axios.get('https://back-immochv2.my-preprod.space/api/immo/v2/public/user_info', {headers: {'Authorization': 'Bearer ' + token}})
+    console.log(user.data.data)
+    instance.SendMessage('MainUI', 'SetUser', JSON.stringify(user.data.data))
 }
