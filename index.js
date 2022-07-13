61467bc2cbf94a5557089f2783f63c0dc81ac8a3
RAKOTONDRASOA Nirilala <nirilala@makeitpulse.in.axian-group.com>
2022-07-13 10:12:36 +0300

    Add token verification

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const app = express()
+const axios = require('axios').default
 
 app.use((req, res, next) => {
-    res.set('Access-Control-Allow-Origin', '*')
+    res.set('Access-Control-Allow-Origin', 'https://back-immochv2.my-preprod.space')
     next()
 })
 
@@ -19,10 +20,14 @@ app.use(express.static('dist', {
 app.get('/login', async (req, res) => {
     try{
         const token = req.headers['authorization'].split('Bearer ')[1]
+        await axios.get('https://back-immochv2.my-preprod.space/api/immo/v2/public/user_info', {headers: {authorization: 'Bearer ' + token}})
         if(!token) throw null
         res.cookie('session', token)
         res.status(201).json({message: 'Logged!'})
-    }catch(e){ res.status(500).json({message: 'An error occured!'}) }
+    }catch(e){
+        if(!e) res.status(500).json({message: 'An error occured!'}) 
+        else res.status(403).json({message: 'Token Rejected!'})
+    }
 })
 
 app.get('/logout', async (req, res) => {
